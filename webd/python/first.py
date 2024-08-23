52bfaef4154db130efe15c3d43b58f1e05202c96
Dipika Mewada <deepikamewada09092000@gmail.com>
2024-08-23 12:19:12 +0530

    Roll NUmber Program

---
+++ webd/python/first.py
@@ -0,0 +1,27 @@
+print("hello, World!")
+x=5
+print(x)
+
+x=int(input("Your roll number= "))
+if x==1:
+    print("Name: Vikash Mewada \n Class: BBA-BIA \n City: Bhopal \n Favourite Player: Messi")
+elif x==2:
+    print("Name: Kamlesh Mewada\nClass: BBa-BIA\n City: Sehore\n Favourite Player: Rohit Sharma")
+elif x==3:
+    print("Name: Pankaj Mewada\nClass: BBA Plain\n City: Sehore\n Favourite Player: Virat Kohli")
+elif x==4:
+    print("Name: Arun Mewada\nClass: MBA\n City: Sehore\n Favourite Player: Rohit SHarma")
+elif x==5:
+    print("Name: Deepak Class: \n City: \n Favourite Player: ")
+elif x==6:
+    print("Name: ANKIT Salvi\nClass: BCA\n City: Udaipur\n Favourite Player: Virat Kohli")
+elif x==7:
+    print("Name: Ahsish Hada\nClass: BBA Plain\n City: Sehore\n Favourite Player: MS Dhoni")
+elif x==8:
+    print("Name: Manish Mewada\nClass: Bcom\n City: Sehore\n Favourite Player: MS Dhoni")
+elif x==9:
+    print("Name: Harsh Rajput\nClass: BBA\n City: Bhopal\n Favourite Player: Rohit Sharma")
+elif x==10:
+    print("Name: Krishna\nClass: Btech\n City: Indore\n Favourite Player: Bumrah")
+else:
+    print("There is Data of ONLY 1 TO 10 STUDENTS")
